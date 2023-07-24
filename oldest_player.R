0a38e2a4aadeebebbc67c32dd53ae465c6dd9d78
sarahwong1008 <123599013+sarahwong1008@users.noreply.github.com>
2023-07-24 18:19:40 -0400

    Update oldest_player.R

--- oldest_player.R
+++ oldest_player.R
@@ -8,5 +8,19 @@
 #' get_oldest_player(2019)
 
 get_oldest_player <- function(year) {
-  # Your code here to find and return the name of the oldest player in the given year.
+  # Filter the dataset to get the players' data for the specified year
+  players_in_year <- subset(nba_data, Year == year)
+
+  if (nrow(players_in_year) == 0) {
+    # No player data found for the given year
+    return(NULL)
+  } else {
+    # Find the oldest player in the specified year
+    oldest_age <- max(players_in_year$Age)
+    oldest_players <- subset(players_in_year, Age == oldest_age)
+
+    # If multiple players have the same age, return the first one (you can sort by any other criteria)
+    oldest_player <- oldest_players$Player[1]
+    return(oldest_player)
+  }
 }
